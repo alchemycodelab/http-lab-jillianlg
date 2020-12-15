4af9a4783c13d901a58d5ad9ac7c605651b4f46d
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-12-14 16:25:07 -0800

    passing test for creates a valid HTTP response

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,19 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+  it('should GET hi', async() => {
+    const res = await request(app)
+      .get('/');
+    expect(res.text).toEqual('hi');
+  });
+
+  it('should POST status code 200 and plain text with the request body', async() => {
+    const res = await request(app)
+      .post('/echo')
+      .send('hi');
+
+    expect(res.text).toEqual('hi');
+  });
 
 });
+
